71bf7132a7d0f3b7c17138270b32b4736eb0c2f7
Sujin Lee <sujinbonnaud@gmail.com>
2023-07-05 18:59:50 -0500

    fix location of main logo

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 //copy menu for mobile
 function copyMenu() {
     //copy inside .dpt-cat to .departments
-    var dptCategory = document.querySelector('.dpt-cat')
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    //copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    //copy .header-top .wrapper to .thetop-nav
+    var topNav = document.querySelector('.header-top');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
 }
 copyMenu();
