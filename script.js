70f6b5780ebe995f6273cab713631c46845da6a2
Sujin Lee <sujinbonnaud@gmail.com>
2023-07-06 15:58:06 -0500

    add plugin(swiper)

--- script.js
+++ script.js
@@ -39,3 +39,27 @@ function toggle(e) {
     if (this.closest('.has-child').classList != 'expand');
     this.closest('.has-child').classList.toggle('expand')
 }
+
+//slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
