8218996d18aa57717be7b73573302fbb7f7d72b9
Sujin Lee <sujinbonnaud@gmail.com>
2023-07-13 15:14:46 -0500

    add hover effect on prodect name

--- script.js
+++ script.js
@@ -42,24 +42,12 @@ function toggle(e) {
 
 //slider
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
     loop: true,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
   });
   
