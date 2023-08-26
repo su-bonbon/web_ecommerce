d00f73470e0edac08b1ee6bbde388f8398d546fd
Sujin Lee <sujinbonnaud@gmail.com>
2023-08-25 20:54:17 -0500

    update js

--- script.js
+++ script.js
@@ -103,9 +103,9 @@ var productBig = new Swiper ('.big-image', {
 var stocks = document.querySelectorAll('.products .stock');
 for (let x = 0; x < stocks.length; x++) {
     let stock = stocks[x].dataset.stock,
-    available = stock[x].querySelector('.qty-available').innerHTML,
-    sold = stock[x].querySelector('.qty-sold'),
+    available = stocks[x].querySelector('.qty-available').innerHTML,
+    sold = stocks[x].querySelector('.qty-sold').innerHTML,
     percent = sold*100/stock;
 
-    stocks[x].querySelector('.availanle').style.width = percent + '%';
+    stocks[x].querySelector('.available').style.width = percent + '%';
 }
