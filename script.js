7315fef947449589613b43d9a116f565fe8457b7
Sujin Lee <sujinbonnaud@gmail.com>
2023-08-27 21:02:53 -0500

    create js function for cart popup

--- script.js
+++ script.js
@@ -109,3 +109,16 @@ for (let x = 0; x < stocks.length; x++) {
 
     stocks[x].querySelector('.available').style.width = percent + '%';
 }
+
+//show cart on click
+const divtoShow = '.mini-cart';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.cart-trigger');
+
+divTrigger.addEventListener('click', () => {
+    setTimeout(() {
+        if(!divPopup.classList.contains('show')) {
+            divPopup.classList.add('show');
+        }
+    }, 250 )
+})
